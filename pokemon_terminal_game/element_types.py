1de8b258235a35a09b87cdc363fa20729e029124
htauL <80499850+htauL@users.noreply.github.com>
2025-04-25 01:08:25 -0300

    [ADD] Updated types resistances and weakness

--- pokemon_terminal_game/element_types.py
+++ pokemon_terminal_game/element_types.py
@@ -22,74 +22,94 @@ class Type(Enum):
 
 # Type chart with weaknesses, resistances, and immunities
 type_chart = {
-    Type.FLYING: {
-        "weaknesses": [Type.ROCK, Type.ELECTRIC, Type.ICE],
-        "resistances": [Type.BUG, Type.FIGHTING, Type.GRASS],
-        "immunities": [Type.GROUND]
-    },
     Type.NORMAL: {
         "weaknesses": [Type.FIGHTING],
         "resistances": [],
         "immunities": [Type.GHOST]
     },
     Type.FIRE: {
-        "weaknesses": [Type.WATER, Type.ROCK],
-        "resistances": [Type.BUG, Type.FIRE, Type.GRASS],
+        "weaknesses": [Type.WATER, Type.GROUND, Type.ROCK],
+        "resistances": [Type.BUG, Type.STEEL, Type.FIRE, Type.GRASS, Type.ICE, Type.FAIRY],
         "immunities": []
     },
     Type.WATER: {
         "weaknesses": [Type.ELECTRIC, Type.GRASS],
-        "resistances": [Type.FIRE, Type.WATER],
-        "immunities": []
-    },
-    Type.GRASS: {
-        "weaknesses": [Type.FIRE, Type.BUG],
-        "resistances": [Type.WATER, Type.GRASS],
+        "resistances": [Type.STEEL, Type.FIRE, Type.WATER, Type.ICE],
         "immunities": []
     },
     Type.ELECTRIC: {
         "weaknesses": [Type.GROUND],
-        "resistances": [Type.ELECTRIC],
+        "resistances": [Type.FLYING, Type.STEEL, Type.ELECTRIC],
         "immunities": []
     },
-    Type.ROCK: {
-        "weaknesses": [Type.WATER, Type.GRASS, Type.FIGHTING],
-        "resistances": [Type.NORMAL, Type.FIRE],
-        "immunities": []
-    },
-    Type.BUG: {
-        "weaknesses": [Type.FIRE, Type.ROCK],
-        "resistances": [Type.GRASS, Type.FIGHTING],
-        "immunities": []
-    },
-    Type.DARK: {
-        "weaknesses": [Type.FIGHTING, Type.BUG],
-        "resistances": [Type.DARK],
-        "immunities": [Type.PSYCHIC]
-    },
-    Type.GROUND: {
-        "weaknesses": [Type.WATER, Type.GRASS],
-        "resistances": [Type.ROCK],
-        "immunities": [Type.ELECTRIC]
-    },
-    Type.FIGHTING: {
-        "weaknesses": [Type.FLYING],
-        "resistances": [Type.BUG, Type.ROCK],
+    Type.GRASS: {
+        "weaknesses": [Type.FIRE, Type.ICE, Type.POISON, Type.FLYING, Type.BUG],
+        "resistances": [Type.GROUND, Type.WATER, Type.GRASS, Type.ELECTRIC],
         "immunities": []
     },
     Type.ICE: {
-        "weaknesses": [Type.FIRE, Type.ROCK],
-        "resistances": [],
+        "weaknesses": [Type.FIRE, Type.FIGHTING, Type.ROCK, Type.STEEL],
+        "resistances": [Type.ICE],
+        "immunities": []
+    },
+    Type.FIGHTING: {
+        "weaknesses": [Type.FLYING, Type.PSYCHIC, Type.FAIRY],
+        "resistances": [Type.ROCK, Type.BUG, Type.DARK],
+        "immunities": []
+    },
+    Type.POISON: {
+        "weaknesses": [Type.GROUND, Type.PSYCHIC],
+        "resistances": [Type.FIGHTING, Type.POISON, Type.BUG, Type.GRASS, Type.FAIRY],
+        "immunities": []
+    },
+    Type.GROUND: {
+        "weaknesses": [Type.WATER, Type.GRASS, Type.ICE],
+        "resistances": [Type.POISON, Type.ROCK],
+        "immunities": [Type.ELECTRIC]
+    },
+    Type.FLYING: {
+        "weaknesses": [Type.ROCK, Type.ELECTRIC, Type.ICE],
+        "resistances": [Type.FIGHTING, Type.BUG, Type.GRASS],
+        "immunities": [Type.GROUND]
+    },
+    Type.PSYCHIC: {
+        "weaknesses": [Type.BUG, Type.GHOST, Type.DARK],
+        "resistances": [Type.FIGHTING, Type.PSYCHIC],
+        "immunities": []
+    },
+    Type.BUG: {
+        "weaknesses": [Type.FLYING, Type.ROCK, Type.FIRE],
+        "resistances": [Type.FIGHTING, Type.GRASS, Type.GROUND],
+        "immunities": []
+    },
+    Type.ROCK: {
+        "weaknesses": [Type.FIGHTING, Type.GROUND, Type.STEEL, Type.WATER, Type.GRASS],
+        "resistances": [Type.NORMAL, Type.FIRE, Type.POISON, Type.FLYING],
         "immunities": []
     },
     Type.GHOST: {
-        "weaknesses": [Type.DARK],
-        "resistances": [],
+        "weaknesses": [Type.GHOST, Type.DARK],
+        "resistances": [Type.POISON, Type.BUG],
         "immunities": [Type.NORMAL, Type.FIGHTING]
     },
-    Type.PSYCHIC: {
-        "weaknesses": [Type.DARK, Type.BUG],
-        "resistances": [],
+    Type.DRAGON: {
+        "weaknesses": [Type.ICE, Type.DRAGON, Type.FAIRY],
+        "resistances": [Type.FIRE, Type.WATER, Type.ELECTRIC, Type.GRASS],
         "immunities": []
+    },
+    Type.DARK: {
+        "weaknesses": [Type.FIGHTING, Type.BUG, Type.FAIRY],
+        "resistances": [Type.GHOST, Type.DARK],
+        "immunities": [Type.PSYCHIC]
+    },
+    Type.STEEL: {
+        "weaknesses": [Type.FIGHTING, Type.GROUND, Type.FIRE],
+        "resistances": [Type.NORMAL, Type.GRASS, Type.ICE, Type.FLYING, Type.PSYCHIC, Type.BUG, Type.ROCK, Type.DRAGON, Type.STEEL, Type.FAIRY],
+        "immunities": [Type.POISON]
+    },
+    Type.FAIRY: {
+        "weaknesses": [Type.POISON, Type.STEEL],
+        "resistances": [Type.FIGHTING, Type.BUG, Type.DARK],
+        "immunities": [Type.DRAGON]
     }
 }
